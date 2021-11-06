a33915d53653ea5e04bbd2d4aaa5dd75250483fc
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-06 13:30:29 -0400

    Creates component and style sheets for Projects, Project, Skills, Contact

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return <div className="contact">Contact Me!</div>;
+};
+
+export default Contact;
---
+++ src/components/Projects/Project.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Projects.css";
+
+const Project = () => {
+  return <div className="projects__single">Singular Project</div>;
+};
+
+export default Project;
