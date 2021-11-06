ec100b1e10cd2ee94e70b19ad42d329a70af1019
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-06 13:23:42 -0400

    Adds color-scheme to App.css. Add each component to the App.js. Changed margin in Index.css from 16 to 0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Intro from "./components/Intro/Intro";
 
 function App() {
-  return <div>Spring Cleaning!</div>;
+  return (
+    <div>
+      <Header />
+      <Intro />
+      <About />
+    </div>
+  );
 }
 
 export default App;
