ff7532514ee75367ffd1b39171dd7271568cc9db
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-08 06:13:32 -0500

    Fetches from db.json in Projects.js and maps out different Project components

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Projects.css";
 
-const Project = () => {
-  return <div className="projects__single">Singular Project</div>;
+const Project = ({ project }) => {
+  return (
+    <div className="projects__single">
+      <div className="projects__single--content">
+        <img
+          src={project.imageUrl}
+          alt={`project gif #${project.id}`}
+          className="projects__single--image"
+        />
+        <div className="projects__single--title">{project.title}</div>
+        <div className="projects__single--description">
+          {project.description}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PacmanLoader } from "react-spinners";
+import Project from "./Project";
 import "./Projects.css";
 
-const Projects = () => {
-  return <div className="projects">// LIST OF PROJECTS</div>;
+const Projects = (props) => {
+  const [projectsData, setProjectsData] = useState({
+    projects: [],
+    loading: true,
+    errors: [],
+  });
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const fetchProjects = async () => {
+    let resp = await fetch("http://localhost:4000/projects");
+    const errorMessage = "YOUR JSON-SERVER IS NOT ON BRO. TURN IT ON!";
+    if (!resp.ok) {
+      setProjectsData({
+        ...projectsData,
+        loading: true,
+        errors: [errorMessage, ...projectsData],
+      });
+      alert(projectsData.errors.join("\n"));
+    }
+
+    let data = await resp.json();
+    setTimeout(() => {
+      setProjectsData({
+        ...projectsData,
+        loading: false,
+        errors: [],
+        projects: data,
+      });
+    }, 1000);
+  };
+  return (
+    <div>
+      <h2 className={"projects__header"}>Projects</h2>
+      <div className="projects">
+        {projectsData.loading ? (
+          <PacmanLoader />
+        ) : (
+          projectsData.projects.map((project) => <Project project={project} />)
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
