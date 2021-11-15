3fe5d2f3fe50d0c63b17c6a3a4d250a5273e6527
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-14 22:55:59 -0500

    Creates css for simulating cube spinning with new about paragraphs

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,20 +4,23 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about">
+      <div className="text-header">About Me</div>
       <div className="about__content">
-        <p>
+        <p className="about__first">
           Excited by solving challenging problems, passionate about creating new
           ideas, inspired by tech’s ability to make information easily and
           readily available.
         </p>
-        <p>
+        <p className="about__second">
           I strive to bring my creations to life. I want to always be learning
-          and be able to create anything that comes to mind. English(preferred)
-          and Fluent Spanish Speaker. Worked in the diverse New York City area
-          using my bi-lingual skills on a daily basis. I am looking for a
-          workplace where I can nurture my Software Engineering skills.
+          and be able to create anything that comes to mind.
         </p>
-        <p>
+        <p className="about__third">
+          Worked in the diverse New York City area using my bi-lingual skills on
+          a daily basis. I am looking for a workplace where I can nurture my
+          Software Engineering skills.
+        </p>
+        <p className="about__fourth">
           I want to continue to be inspired by how code can help shape our
           world.
         </p>
