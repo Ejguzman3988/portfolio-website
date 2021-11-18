ea699f9c071ceeb7f757fa320a85e85d920adbb4
ejguzman3988 <eriberto.guzman@outlook.com>
2021-11-18 06:31:06 -0500

    Updates wording for about me and fixes some sizing issues with the smaller screens.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,18 +7,17 @@ const About = () => {
       <div className="text-header">About Me</div>
       <div className="about__content">
         <p className="about__first">
-          Excited by solving challenging problems, passionate about creating new
-          ideas, inspired by tech’s ability to make information easily and
+          Excited by solving challenging problems. Passionate about creating new
+          ideas. Inspired by tech’s ability to make information easily and
           readily available.
         </p>
         <p className="about__second">
-          I strive to bring my creations to life. I want to always be learning
-          and be able to create anything that comes to mind.
+          Passionate about learning, bringing ideas to life, and creating
+          anything that comes to mind.
         </p>
         <p className="about__third">
-          Worked in the diverse New York City area using my bi-lingual skills on
-          a daily basis. I am looking for a workplace where I can nurture my
-          Software Engineering skills.
+          I am looking for a workplace where I can nurture my Software
+          Engineering skills.
         </p>
         <p className="about__fourth">
           I want to continue to be inspired by how code can help shape our
